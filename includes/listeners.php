599b82bf3e384d81b76de630e044f02f6d1b2516
Otto Rask <ojrask@gmail.com>
2016-01-01 01:36:43 +0200

    Additional listener registration: post meta, user auth.

--- includes/listeners.php
+++ includes/listeners.php
@@ -14,5 +14,17 @@ return [
     /**
      * Posts, pages and CPTs.
      */
-    'wp_insert_post' => Posts\PostListener::class
+    'wp_insert_post' => Posts\PostListener::class,
+
+    /**
+     * Post meta, post type independent.
+     */
+    'updated_post_meta' => Posts\PostMetaListener::class,
+
+    /**
+     * User login/logout.
+     */
+    'wp_login' => Auth\AuthListener::class,
+    'clear_auth_cookie' => Auth\AuthListener::class
+
 ];
