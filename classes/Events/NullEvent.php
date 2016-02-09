dc9add13347070695231a97085a2e223d99a28ac
Otto Rask <ojrask@gmail.com>
2016-02-09 17:03:47 +0200

    Add NullEvent for null logging.

---
+++ classes/Events/NullEvent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Wplog\Events;
+
+/**
+ * Class NullEvent
+ *
+ * NullEvents for null logging.
+ *
+ * @since 0.1.0
+ * @package Wplog\Events
+ */
+class NullEvent extends Event {}
